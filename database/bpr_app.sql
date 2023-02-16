1b468f6f64982fa3b9cd9021e29f99328b85abe2
Dandi Hermawan <dandihermawan87@gmail.com>
2023-02-16 13:34:51 +0700

    dikit lagi finishing

--- database/bpr_app.sql
+++ database/bpr_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2023 at 10:57 AM
+-- Generation Time: Feb 16, 2023 at 07:33 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
